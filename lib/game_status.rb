0dde97435b87cd9e83ae30c1d49f44ba9a296a4f
Elle Faraday <faraday.elle@gmail.com>
2017-10-15 18:04:23 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -17,24 +17,23 @@ WIN_COMBINATIONS = [
 
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 def won?(board)
-  token_check = []
-  winning_array = []
-  WIN_COMBINATIONS.each do |win_condition|
-    win_condition.each do |place|
-      token_check << board[place]
-    end
-    if token_check.all? {|token| "X"}
-      winning_array = win_condition
-    elsif token_check.all? {|token| "O"}
-      winning_array = win_condition
+  WIN_COMBINATIONS.each do |win_combination|
+    win_index_1 = win_combination[0]
+    win_index_2 = win_combination[1]
+    win_index_3 = win_combination[2]
+
+    position_1 = board[win_index_1]
+    position_2 = board[win_index_2]
+    position_3 = board[win_index_3]
+
+    if position_1 == "X" && position_2 == "X" && position_3 == "X"
+      return win_combination
+    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+      return win_combination
     else
+      false
     end
-    token_check = []
   end
-    if winning_array.empty?
-    else
-      winning_array
-    end
 end
+
 puts won?(board)
-puts winning_array
