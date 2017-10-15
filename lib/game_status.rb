d879109b75ae58e76280752ccef5bf42aa5e4f55
Elle Faraday <faraday.elle@gmail.com>
2017-10-15 18:14:34 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -25,14 +25,18 @@ def won?(board)
     position_1 = board[win_index_1]
     position_2 = board[win_index_2]
     position_3 = board[win_index_3]
+
+    if position_1 == "X" && position_2 == "X" && position_3 == "X"
+      winning_array = win_combination
+    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+      winning_array = win_combination
+    else
+    end
   end
-  if position_1 == "X" && position_2 == "X" && position_3 == "X"
-    return win_combination
-  elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
-    return win_combination
-  else
+  if winning_array.empty?
     false
-  end
+  else
+    winning_array
 end
 
 puts won?(board)
